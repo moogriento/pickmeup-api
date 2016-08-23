67ed65f2bc5182e41aa90aab79e772a8a6fc03f0
Perez, Juan <Juan.Perez1@teamaol.com>
2016-08-23 10:40:17 -0500

    [develop] swagger /api/routes model change; Route change.

--- models/route.model.js
+++ models/route.model.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 
 var RouteSchema = new mongoose.Schema({
-    departure: {type:String, require:true},
-    arrival: {type:String, require:true},
+    departure: {type:String, require:true, enum: ['BELATRIX_SAN_ISIDRO', 'BELATRIX_LA_MOLINA']},
+    arrival: {type:String, require:true, enum: ['SAN_ISIDRO', 'LINCE', 'CALLAO', 'CHORRILLOS', 'SAN_BORJA', 'CERCADO_DE_LIMA']},
     cost: Number,
     payment_type: String,
-    departure_time: Date,
-    contact: String,
-    streets: Array,
-    seats: Number,
-    passengers: Array
+    routeOwner: String,
+    departure_time: { type: Date, default: Date.now },
+    placeAvailable: Number,
+    seats: String,
+    addess_destination: Array
 });
 
 RouteSchema.plugin(autoIncrement.plugin, {model: 'Route', field: 'id', startAt: 1});
